9cf4e80ec07cbc94fd55de5fec6a5f04a161ab09
armandoltx <armando@armandoltx.com>
2019-07-04 16:27:34 +1000

    Actualizando el state del formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -3,6 +3,34 @@ import React, { useState, Fragment } from 'react';
 
 function Formulario () {
 
+  // añadimos el state, el 1 es el state(cita), el 2 es q va actualizar el state (actualizarCita) y dento de los parentesis son los valores iniciales dentro del {}
+  // para acceder a los valores del formulario antes usabamos ref, ahora usamos la propiedad name del input
+  const [cita, actualizarCita] = useState({
+    mascota: '',
+    propietario: '',
+    fecha: '',
+    hora: '',
+    sintomas: '',
+  })
+
+  const actualizarState = (e) => {
+    // 1. Prevenir default para q no se recargue
+    // 2. Crear el Objeto con los datos
+    // 3. Agregarlo y enviarlo por props hacia el padre para actualizar el state
+    // 4. Resetear el formulario
+    // 5. Elimine el error
+
+    // actualizar el state con los nuevos datos del formulario
+    // primero hacemos copia del state, para q cuando escribamos no nos borre los otros campos
+    // con [e.target.name] accedemos al name en cada input
+    actualizarCita({
+      ...cita,
+      [e.target.name] : [e.target.value]
+    })
+  }
+
+  console.log(cita);
+
   return (
     <Fragment>
       <form>
@@ -12,13 +40,14 @@ function Formulario () {
           name="mascota"
           className="u-full-width"
           placeholder="Nombre de la Mascota"
+          onChange={actualizarState}
         />
 
         <label>Nombre del Dueño</label>
         <input
           type="text"
           name="propietario"
-          className="u-fill-width"
+          className="u-full-width"
           placeholder="Nombre del dueño de la Mascota"
         />
 
