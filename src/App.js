5736f388bc742e6d58d1cd9fb85ec688d7e98ac6
armandoltx <armando@armandoltx.com>
2019-07-10 13:42:21 +1000

    Almacenando las citas en el localstorage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 
 import Formulario from './components/Formulario';
 import Cita from './components/Cita';
 
 function App() {
-  // useState retorna 2 funciones el state y la funcion q actualiza el state
+  // al recargar la pagina se pierden las citas, eso pasa pq el state inicial es un [] (viene de useState([]);) por lo que hay q pasar el valor q tenemos en localStorage. Para ello cargamos las citas de localStorage como state inicial.
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
 
+  if (!citasIniciales) {
+    citasIniciales = []
+  }
+
+
+  // useState retorna 2 funciones el state y la funcion q actualiza el state
   // ====> const [state, actualizarState] = useState();  <===
-  
   // El 1 state = this.state
   // El 2 es la funcion q actualiza el state == this.setState({});
   // El 3 useState, toma el valor inicial del state, puede ser un valor, un array o un objeto...
@@ -16,7 +22,7 @@ function App() {
   //  nombre: '',
   //  apellido: '',
   //  });
-  const [citas, guardarCita] = useState([]);
+  const [citas, guardarCita] = useState(citasIniciales);
 
   // console.log(citas);
 
@@ -39,6 +45,27 @@ function App() {
     guardarCita(nuevasCitas); // actualizamos el state
   }
 
+  // Similar to didMount y didUpdated ejecuta un arrow function
+  // useEffect se ejecuta cada vez q el componente cambie en cualquier cosa, podemos controlar eso pasandole un segundo parametro diciendole lo q tiene q cambiar para q se ejecute, lo q tiene q leer
+  // en nuestro caso se ejecutara cuando las citas( el state) tenga algun cambio
+  useEffect(
+    () => {
+      // console.log("dentro de useEffect, componente listo o algo cambio");
+      // para q se guarden en el localstorage:
+      let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+      // lo nombramos citas | leemos el dato de localStorage y lo pasamos por JSON.parse para convertirlo a JSON El método JSON.stringify() convierte un objeto o valor de JavaScript en una cadena de texto JSON
+      // para ver el storage chrome console > Application
+
+      if(citasIniciales) {
+        localStorage.setItem('citas', JSON.stringify(citas)); // estas citas son las del state que las convertimos a string
+      } else {
+        localStorage.setItem('citas', JSON.stringify([]));
+      }
+    },[citas] );
+
+  // Cargar Condicionalmente el Titulo
+  const titulo = Object.keys(citas).length === 0 ? 'No hay Citas' : 'Administrar las Citas';
+
   return(
     <Fragment>
       <h1>Administrador de Pacientes</h1>
@@ -47,6 +74,7 @@ function App() {
           <Formulario crearCita={crearCita} />
         </div>
         <div className="one-half column">
+          <h2>{titulo}</h2>
           {citas.map((cita, index) => (
             <Cita
               key={index}
